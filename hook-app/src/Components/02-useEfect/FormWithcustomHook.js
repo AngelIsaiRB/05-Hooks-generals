560207dc8fbf2a58ae05b6be71b72e3b3e4b32a4
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-07 16:27:07 -0600

    useForm custom hook

---
+++ hook-app/src/Components/02-useEfect/FormWithcustomHook.js
@@ -0,0 +1,65 @@
+import React, { useEffect } from 'react'
+import { useForm } from '../../hooks/useForm';
+import "./effects.css"
+
+
+// ejecuta un efecto cuando algo afecta nuestros componentes
+export const FormWithCustomHook = () => {
+    
+    const  [formValues, handleInputChange] = useForm({
+        name:"",
+        email:"",
+        password:""
+    });
+    const {name, email,password}=formValues;
+    
+    useEffect(()=>{
+        console.log("email cambio");
+    },[]);
+
+    const hanldeSubmit=(e)=>{
+        e.preventDefault();
+        console.log(formValues);
+    }
+
+    return (
+        <form onSubmit={hanldeSubmit}>
+            <h1>FormWhit custom hook</h1>   
+            <hr/>
+            <div className="form-group">
+                <input 
+                type="text" 
+                name="name" 
+                className="form-control"
+                placeholder="Tu nombre"
+                autoComplete="off"
+                value={name}
+                onChange={ handleInputChange}
+                />
+            </div>
+            <div className="form-group">
+                <input 
+                type="text" 
+                name="email" 
+                className="form-control"
+                placeholder="email@ejemplo.com"
+                autoComplete="off"
+                value={email}
+                onChange={ handleInputChange}
+                />
+            </div>
+            <div className="form-group">
+                <input 
+                type="password" 
+                name="password" 
+                className="form-control"
+                placeholder="**"
+                autoComplete="off"
+                value={password}
+                onChange={ handleInputChange}
+                />
+            </div>
+            <button type="submit" className="btn btn-primary"> Guardar</button>
+        </form>
+    )
+}
--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CounterApp } from './Components/01-UseState/CounterApp';
 import { CounterwithCustomHooks } from './Components/01-UseState/CounterwithCustomHooks';
+import { FormWithCustomHook } from './Components/02-useEfect/FormWithcustomHook';
 import { SimpleForm } from './Components/02-useEfect/SimpleForm';
 // import { HooksApp } from './HooksApp';
 
 
 ReactDOM.render(
  
-    <SimpleForm />,
+    <FormWithCustomHook />,
  
   document.getElementById('root')
 );

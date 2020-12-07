e1eaaa3bda72f8664695b16b83eeb55920671e9d
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-07 16:03:37 -0600

    useEffect cautions

--- hook-app/src/Components/02-useEfect/Message.js
+++ hook-app/src/Components/02-useEfect/Message.js
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
-
+    const [coords, setCoords] = useState({x:0,y:0})
+    const {x,y}=coords;
     useEffect(() => {
-        // console log
+        
+        const mouseMove = (e)=>{
+            const coors= {x:e.x, y:e.y};
+            setCoords(coors)
+        }
+        window.addEventListener("mousemove", mouseMove);
         return () => {
-            console.log("componentes desmontado")
+            window.removeEventListener("mousemove", mouseMove);
         }
     }, [])
     return (
         <div>
             <h3>genail</h3>
+            <p>
+                x: {x} y: {y}
+            </p>
         </div>
     )
 }

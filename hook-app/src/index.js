a68ec4f097c63c83d24639cc906387a80919ed3a
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-08 19:30:58 -0600

    init todoApp

--- hook-app/src/index.js
+++ hook-app/src/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './Components/01-UseState/CounterApp';
-import { CounterwithCustomHooks } from './Components/01-UseState/CounterwithCustomHooks';
-import { FormWithCustomHook } from './Components/02-useEfect/FormWithcustomHook';
-import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
-import { SimpleForm } from './Components/02-useEfect/SimpleForm';
-import { FocusScreen } from './Components/04-useRef/FocusScreen';
-import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
-import { LayoutEffect } from './Components/05-useLayoutEffect/LayoutEffect';
-import { Memorize } from './Components/06-memos/Memorize';
-import { MemoHook } from './Components/06-memos/MemoHook';
-import { CallbackHook } from './Components/06-memos/CallbackHook';
-import { Padre } from './Components/07-work/Padre';
+// import { CounterApp } from './Components/01-UseState/CounterApp';
+// import { CounterwithCustomHooks } from './Components/01-UseState/CounterwithCustomHooks';
+// import { FormWithCustomHook } from './Components/02-useEfect/FormWithcustomHook';
+// import { MultipleCustomHooks } from './Components/03-examples/MultipleCustomHooks';
+// import { SimpleForm } from './Components/02-useEfect/SimpleForm';
+// import { FocusScreen } from './Components/04-useRef/FocusScreen';
+// import { RealExampleRef } from './Components/04-useRef/RealExampleRef';
+// import { LayoutEffect } from './Components/05-useLayoutEffect/LayoutEffect';
+// import { Memorize } from './Components/06-memos/Memorize';
+// import { MemoHook } from './Components/06-memos/MemoHook';
+// import { CallbackHook } from './Components/06-memos/CallbackHook';
+// import { Padre } from './Components/07-work/Padre';
+import { TodoApp } from './Components/08-useReducer/TodoApp';
 // import { HooksApp } from './HooksApp';
 
 
 ReactDOM.render(
  
-    <Padre />,
+    <TodoApp />,
  
   document.getElementById('root')
 );
+// import "./Components/08-useReducer/intro-reducer";

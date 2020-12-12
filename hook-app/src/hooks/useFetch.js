03a8d400160715cfc8b093785979708b71cb1601
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-12 16:42:16 -0600

    test userFth

--- hook-app/src/hooks/useFetch.js
+++ hook-app/src/hooks/useFetch.js
@@ -28,6 +28,13 @@ export const useFetch = (url) => {
             });
         }
        })
+       .catch(()=>{
+           setState({
+               data:null,
+               loading:false,
+               error: "no se pudo cargar la informacion"
+           })
+       })
    }, [url])
 
    return state;

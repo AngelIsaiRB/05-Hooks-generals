6b759a7d9796d35609f481d91e6496e0b916a4a5
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-11 22:32:15 -0600

    test UseCounter

--- hook-app/src/tests/hooks/usecounter.test.js
+++ hook-app/src/tests/hooks/usecounter.test.js
@@ -1,4 +1,4 @@
-const { renderHook } = require("@testing-library/react-hooks");
+const { renderHook, act } = require("@testing-library/react-hooks");
 const { UseCouter } = require("../../hooks/UseCouter");
 
 describe("pruebas en useCounter",()=>{
@@ -10,7 +10,46 @@ describe("pruebas en useCounter",()=>{
         expect(typeof result.current.increment).toBe("function");
         expect(typeof result.current.decrement).toBe("function");
         expect(typeof result.current.reset).toBe("function");
-    })
+    });
+    
+    test('should de tener el counter en 100', () => {
+        const { result }= renderHook(()=>UseCouter(100));
+
+        expect(result.current.state).toBe(100);
+        
+        
+    });
+
+    test('should de incrementar el counter en 1', () => {
+        const {result} = renderHook(()=>UseCouter(100));
+        const {increment} = result.current;
+        act(()=>{
+            increment(1);
+        });
+        const {state} =result.current;
+        expect(state).toBe(101)
+    });
+    test('should de decremntar el counter en 1', () => {
+        const {result} = renderHook(()=>UseCouter(100));
+        const {decrement} = result.current;
+        act(()=>{
+            decrement(1);
+        });
+        const {state} =result.current;
+        expect(state).toBe(99)
+    });
+    test('should de restablecerce el valor a 100', () => {
+        const {result} = renderHook(()=>UseCouter(100));
+        const {increment, reset} = result.current;
+        act(()=>{
+            increment(10);
+            reset();
+        });
+        const {state} =result.current;
+        expect(state).toBe(100)
+    });
+    
+
     
 
 });

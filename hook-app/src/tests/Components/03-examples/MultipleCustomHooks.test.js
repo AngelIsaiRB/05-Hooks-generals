d3c1ad06e6dc8db14fbc2de162d665b4326870f8
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2020-12-12 21:13:54 -0600

    test hooks simultaneos

---
+++ hook-app/src/tests/Components/03-examples/MultipleCustomHooks.test.js
@@ -0,0 +1,56 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+import { MultipleCustomHooks } from '../../../Components/03-examples/MultipleCustomHooks';
+import { UseCouter } from '../../../hooks/UseCouter';
+import { useFetch } from '../../../hooks/useFetch';
+
+jest.mock('../../../hooks/useFetch');
+jest.mock('../../../hooks/UseCouter');
+
+
+describe('test en multiple custom hooks', () => {
+    beforeEach( () =>  {
+
+        UseCouter.mockReturnValue({
+            state:10,
+            increment:()=>{}
+        });
+
+    })
+
+    test('should de mostrarse correctamente', () => {
+
+ 
+
+            useFetch.mockReturnValue({
+                data:null,
+                loading:true,
+                error:null
+            });
+
+
+        const wrapper = shallow(<MultipleCustomHooks/>);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('should de mostrar la informacion ', () => {
+       
+        
+          useFetch.mockReturnValue({
+                data:[{
+                    author:"isai",
+                    quote:"hola mundo"
+                }],
+                loading:true,
+                error:null
+            }); 
+        const wrapper = shallow(<MultipleCustomHooks/>);
+        
+        expect(wrapper.find(".alert").exists()).toBe(false);
+        expect(wrapper.find(".mb-0").text().trim()).toBe("hola mundo");
+        expect(wrapper.find("footer").text().trim()).toBe("isai");
+
+    })
+    
+    
+})
